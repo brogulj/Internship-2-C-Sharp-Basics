6fe2a7710bef86c27566de793b92b84a5a55f81d
brogulj <brunoigre2009@gmail.com>
2020-11-29 00:03:48 +0100

    brogulj_zavrsni-commit

--- Program.cs
+++ Program.cs
@@ -32,9 +32,9 @@ namespace playlistDump
                     System.Console.WriteLine("Jeste li sigurni da želite odabrati opciju broj "+odabir);
                     System.Console.WriteLine("pritisnite d za da ili n za ne");
                     dane=Console.ReadLine();
-                    if(dane=="n")
-                    return false;
+                    if(dane=="d") 
                     return true;
+                    return false;
         }
         public static bool Menu(Dictionary<int, string> dict){
             Boolean palivozi= true;                                                                //palivozi isto bool, koristimo ga da odredimo nastavi poslije zavrsene funkcije
@@ -50,10 +50,7 @@ namespace playlistDump
             System.Console.WriteLine(" 9 - Uređivanje rednog broja pjesme");
             System.Console.WriteLine("10 - Shuffle liste");
             System.Console.WriteLine(" 0 - Izlaz iz aplikacije");
-            var odabir = int.Parse(Console.ReadLine());                                           //tempInt, tempInt2 i tempStr su nekakvi placeholderi koje koristim u vecini opcija u zadatku
-            var tempInt=0;
-            var tempInt2=0;
-            var tempStr="";
+            var odabir = int.Parse(Console.ReadLine());                                           
             var dane=new Boolean();
             switch(odabir){
                 case 0:
@@ -66,102 +63,43 @@ namespace playlistDump
                     dane=DaNe(odabir);
                     if(!dane)
                     break;
-                    System.Console.WriteLine("Playlista sadrzi slijedece pjesme");
-                    foreach(var pair in dict)
-                    {
-                        Console.WriteLine(pair.Key+". "+pair.Value);
-                    }
+                    IspisCijelePlayliste(dict);
                     break;
                 case 2:
                     dane=DaNe(odabir);
                     if(!dane)
                     break;
-                    System.Console.WriteLine("Unesite redni broj pjesme koje zelite da se ispise: ");
-                    tempInt = int.Parse(Console.ReadLine());
-                    if(tempInt>dict.Count || tempInt<1){
-                        System.Console.WriteLine("Unijeli ste redni broj koji nije na listi");
-                        break;
-                    }
-                    System.Console.WriteLine("Pjesma pod rednim brojem "+tempInt+" je "+dict[tempInt]+".");
+                    IspisPoRednomBroju(dict);
                     break;
                 case 3:
                     dane=DaNe(odabir);
                     if(!dane)
                     break;
-                    System.Console.WriteLine("unesite ime pjesme ciji redni broj zelite da se ispise");
-                    tempStr = Console.ReadLine();
-                    foreach(var pair in dict){                                                  //foreach petlja koja trazi indeks po imenu pjesme
-                        if(pair.Value==tempStr){
-                            tempInt=pair.Key;
-                            break;
-                        }
-                    }
-                    if(tempInt==0){
-                        System.Console.WriteLine("Unijeli ste ime pjesme koja se ne nalazi na listi");
-                        break;
-                    }
-                    System.Console.WriteLine("Redni broj pjesme "+ tempStr + " je "+ tempInt);
+                    IspisRednogBroja(dict);
                     break;
                 case 4:
                     dane=DaNe(odabir);
                     if(!dane)
                     break;
-                    System.Console.WriteLine("Unesite ime pjesme koju želite dodati na playlistu");
-                    tempStr=Console.ReadLine();
-                    
-                    foreach(var pair in dict){
-                        while(pair.Value== tempStr){                                            //while petlja iz koje necemo izac ako ne damo ime pjesme koje ne postoji na playlisti
-                            System.Console.WriteLine("Ta pjesma već postoji na playlisti, molimo vas unesite drugo ime");
-                            tempStr=Console.ReadLine();
-                        }
-                    }
-                    dict[dict.Count+1]=tempStr;
-                    System.Console.WriteLine("Pjesma je uspjesno dodana na playlistu!");
+                    DodavanjeNaPlaylistu(dict);
                     break;
                 case 5:
                     dane=DaNe(odabir);
                     if(!dane)
                     break;
-                    System.Console.WriteLine("Unesite redni broj pjesme koju želite obrisati: "); 
-                    tempInt=int.Parse(Console.ReadLine());
-                    if(tempInt>dict.Count || tempInt<1){
-                        System.Console.WriteLine("Unijeli ste redni broj koji nije na listi");
-                        break;
-                    }
-                    for(var i=tempInt;i<dict.Count;i++){
-                        dict[i]=dict[i+1];
-                    }
-                    dict.Remove(dict.Count);
-                    System.Console.WriteLine("Pjesma je uspjesno obrisana sa playiste");
+                    BrisanjePoBroju(dict);
                     break;
                 
                 case 6:
                     dane=DaNe(odabir);
                     if(!dane)
                     break;
-                    System.Console.WriteLine("Unesite ime pjesme koju zelite obrisati: ");
-                    tempStr=Console.ReadLine();
-                    foreach(var pair in dict){
-                        if(pair.Value==tempStr){
-                            tempInt=pair.Key;
-                            break;
-                        }
-                    }
-                    if(tempInt==0){
-                        System.Console.WriteLine("Unijeli ste ime pjesme koja se ne nalazi na listi");
-                        break;
-                    }
-                    for(var i=tempInt;i<dict.Count;i++){
-                        dict[i]=dict[i+1];
-                    }
-                    dict.Remove(dict.Count);
-                    System.Console.WriteLine("Pjesma je uspjesno obrisana sa playiste");
+                    BrisanjePoImenu(dict);
                     break;
                 case 7:
                     dane=DaNe(odabir);
                     if(!dane)
                     break;
-                    System.Console.WriteLine("Cijela lista je obrisana");
                     dict.Clear();
                     System.Console.WriteLine("Cijela lista je obrisana");
                     break;
@@ -169,11 +107,73 @@ namespace playlistDump
                     dane=DaNe(odabir);
                     if(!dane)
                     break;
+                    PromjeniImePjesme(dict);
+                    break;
+                case 9:
+                    dane=DaNe(odabir);
+                    if(!dane)
+                    break;
+                    PromjeniRedniBroj(dict);
+                    break;
+                case 10:                                                                    
+                    dane=DaNe(odabir);
+                    if(!dane)
+                    break;
+                    Shuffle(dict);
+                    break;
+            }
+            return palivozi;
+
+
+        }
+        //tempInt, tempInt2 i tempStr su nekakvi placeholderi koje koristim u vecini opcija u zadatku
+        public static void Shuffle(Dictionary<int,string> dict){
+            var tempStr="";
+            var tempInt=0;
+            var random=new Random();
+                    for(int i=1;i<=dict.Count;i++){
+                        tempInt=random.Next(1,dict.Count+1);
+                        tempStr=dict[i];
+                        dict[i]=dict[tempInt];
+                        dict[tempInt]=tempStr;
+
+                    }
+                    System.Console.WriteLine("Playlista je shuffleana!");
+        }
+        public static void PromjeniRedniBroj(Dictionary<int,string> dict){
+                    var tempStr="";
+                    var tempInt=0;
+                    var tempInt2=0;
+                    System.Console.WriteLine("Unesite ime pjesme kojoj zelite promjeniti redni broj");
+                    tempStr=Console.ReadLine();
+                    while(tempInt==0){
+                        foreach(var pair in dict){
+                            if(pair.Value==tempStr){
+                                tempInt=pair.Key;
+                                break;
+                            }
+                        }
+                        if(tempInt==0){
+                            System.Console.WriteLine("Unijeli ste ime pjesme koja se ne nalazi na listi");
+                            System.Console.WriteLine("Molimo vas unesite novo ime pjesme: ");
+                            tempStr=Console.ReadLine();
+                        }
+                    }
+                    System.Console.WriteLine("Unesite novi redni broj veći od 0 i manji od "+dict.Count);
+                    tempInt2=int.Parse(Console.ReadLine());
+                    dict[tempInt]=dict[tempInt2];
+                    dict[tempInt2]=tempStr;
+                    System.Console.WriteLine("Uspješno uređen redni broj");
+        }
+        public static void PromjeniImePjesme(Dictionary<int,string> dict){
+                    var tempStr="";
+                    var tempInt=0;
                     System.Console.WriteLine("Unesite redni broj pjesme kojoj zelite urediti ime: ");
                     tempInt=int.Parse(Console.ReadLine());
-                    if(tempInt>dict.Count || tempInt<1){
+                    while(tempInt>dict.Count || tempInt<1){
                         System.Console.WriteLine("Unijeli ste redni broj koji nije na listi");
-                        break;
+                        System.Console.WriteLine("Unesite redni broj pjesme kojoj zelite urediti ime: ");
+                        tempInt=int.Parse(Console.ReadLine());
                     }
                     System.Console.WriteLine("Unesite novo ime pjesme za koju žeite da bude na rednom mjestu "+ tempInt);
                     tempStr=Console.ReadLine();
@@ -185,48 +185,104 @@ namespace playlistDump
                     }
                     dict[tempInt]=tempStr;
                     System.Console.WriteLine("Uspješno uređeno ime");
-                    break;
-                case 9:
-                    dane=DaNe(odabir);
-                    if(!dane)
-                    break;
-                    System.Console.WriteLine("Unesite ime pjesme kojoj zelite promjeniti redni broj");
-                    tempStr=Console.ReadLine();
-                    foreach(var pair in dict){
-                        if(pair.Value==tempStr){
-                            tempInt=pair.Key;
-                            break;
-                        }
-                    }
-                    if(tempInt==0){
-                        System.Console.WriteLine("Unijeli ste ime pjesme koja se ne nalazi na listi");
-                        break;
-                    }
-                    System.Console.WriteLine("Unesite novi redni broj");
-                    tempInt2=int.Parse(Console.ReadLine());
-                    dict[tempInt]=dict[tempInt2];
-                    dict[tempInt2]=tempStr;
-                    System.Console.WriteLine("Uspješno uređen redni broj");
-                    break;
-                case 10:                                                                    
-                    dane=DaNe(odabir);
-                    if(!dane)
-                    break;
-                    var random=new Random();
-                    for(int i=1;i<=dict.Count;i++){
-                        tempInt=random.Next(1,dict.Count+1);
-                        tempStr=dict[i];
-                        dict[i]=dict[tempInt];
-                        dict[tempInt]=tempStr;
-
-                    }
-                    System.Console.WriteLine("Playlista je shuffleana!");
-                    break;
-            }
-            return palivozi;
-            
 
         }
-        
+        public static void BrisanjePoImenu(Dictionary<int,string> dict){
+                    var tempStr="";
+                    var tempInt=0;
+                    System.Console.WriteLine("Unesite ime pjesme koju zelite obrisati: ");
+                    tempStr=Console.ReadLine();
+                    while(tempInt==0){
+                        foreach(var pair in dict){
+                            if(pair.Value==tempStr){
+                                tempInt=pair.Key;
+                                break;
+                            }
+                        }
+                        if(tempInt==0){
+                            System.Console.WriteLine("Unijeli ste ime pjesme koja se ne nalazi na listi");
+                            System.Console.WriteLine("Molimo vas unesite nov ime: ");
+                            tempStr=Console.ReadLine();
+                        }
+                    }
+                    for(var i=tempInt;i<dict.Count;i++){
+                        dict[i]=dict[i+1];
+                    }
+                    dict.Remove(dict.Count);
+                    System.Console.WriteLine("Pjesma je uspjesno obrisana sa playiste");
+
+        }
+        public static void BrisanjePoBroju(Dictionary<int,string> dict){
+                var tempInt=0;
+
+                System.Console.WriteLine("Unesite redni broj pjesme koju želite obrisati: "); 
+                tempInt=int.Parse(Console.ReadLine());
+                while(tempInt>dict.Count || tempInt<1){
+                    System.Console.WriteLine("Unijeli ste redni broj koji nije na listi");
+                    System.Console.WriteLine("Molimo vas unesite novi redni broj:");
+                    tempInt=int.Parse(Console.ReadLine());
+                }
+                for(var i=tempInt;i<dict.Count;i++){
+                    dict[i]=dict[i+1];
+                }
+                dict.Remove(dict.Count);
+                System.Console.WriteLine("Pjesma je uspjesno obrisana sa playiste");
+
+        }
+        public static void DodavanjeNaPlaylistu(Dictionary<int,string> dict){
+                var tempStr="";
+                System.Console.WriteLine("Unesite ime pjesme koju želite dodati na playlistu");
+                tempStr=Console.ReadLine();
+                
+                foreach(var pair in dict){
+                    while(pair.Value== tempStr){                                            //while petlja iz koje necemo izac ako ne damo ime pjesme koje ne postoji na playlisti
+                        System.Console.WriteLine("Ta pjesma već postoji na playlisti, molimo vas unesite drugo ime");
+                        tempStr=Console.ReadLine();
+                    }
+                }
+                dict[dict.Count+1]=tempStr;
+                System.Console.WriteLine("Pjesma je uspjesno dodana na playlistu!");
+
+        }
+        public static void IspisRednogBroja(Dictionary<int,string> dict){
+            var tempInt=0;
+            var tempStr="";
+            System.Console.WriteLine("unesite ime pjesme ciji redni broj zelite da se ispise");
+            tempStr = Console.ReadLine();
+            while(tempInt==0){
+                foreach(var pair in dict){                                                  //foreach petlja koja trazi indeks po imenu pjesme
+                    if(pair.Value==tempStr){
+                        tempInt=pair.Key;
+                        break;
+                    }
+                }
+                if(tempInt==0){
+                    System.Console.WriteLine("Unijeli ste ime pjesme koja se ne nalazi na listi");
+                    System.Console.WriteLine("Molimo vas unesite novo ime:");
+                    tempStr=Console.ReadLine();
+                }
+            }
+            System.Console.WriteLine("Redni broj pjesme "+ tempStr + " je "+ tempInt);
+
+        }
+        public static void IspisPoRednomBroju(Dictionary<int,string> dict){
+            var tempInt=0;
+            System.Console.WriteLine("Unesite redni broj pjesme koje zelite da se ispise: ");
+            tempInt = int.Parse(Console.ReadLine());
+            while(tempInt>dict.Count || tempInt<1){
+                System.Console.WriteLine("Unijeli ste redni broj koji nije na listi");
+                System.Console.WriteLine("Molimo vas unesite novi redni broj:");
+                tempInt=int.Parse(Console.ReadLine());
+            }
+            System.Console.WriteLine("Pjesma pod rednim brojem "+tempInt+" je "+dict[tempInt]+".");
+
+        }
+        public static void IspisCijelePlayliste(Dictionary<int,string> dict){
+            System.Console.WriteLine("Playlista sadrzi slijedece pjesme");
+            foreach(var pair in dict)
+            {
+                Console.WriteLine(pair.Key+". "+pair.Value);
+            }
+        }
     }
 }
